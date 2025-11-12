da757b87dbd8614733592fbd24a6aa539813defa
Gunti Nikitha <Gunti Nikitha@LAPTOP-ER6FN7S0>
2025-11-12 21:49:01 +0530

    car class car main

---
+++ tns.backend.training/src/tns/backend/training/car.java
@@ -0,0 +1,15 @@
+package tns.backend.training;
+
+public class car {
+	//variables 
+	int speed;
+	String model;
+	String name;
+	//methods
+	void start() {
+		
+	}
+	
+	
+
+}
---
+++ tns.backend.training/src/tns/backend/training/carmain.java
@@ -0,0 +1,13 @@
+package tns.backend.training;
+
+public class carmain {
+
+	public static void main(String[] args) {
+		//create instance of a class
+		//classname objectname = new constructor();
+		car c 
+	
+
+	}
+
+}
