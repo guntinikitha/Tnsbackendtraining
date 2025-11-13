0b341bfa3436f297c558322c3c1d5d5b6bee8fea
Gunti Nikitha <Gunti Nikitha@LAPTOP-ER6FN7S0>
2025-11-13 18:20:44 +0530

    push the code

---
+++ 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/tns.backend.training/src/tns/backend/training/bike.java
@@ -0,0 +1,13 @@
+// demo for class structure
+// A class gives a blueprint
+public class bike {
+    // variables
+    int speed;
+    String name;
+    String model;
+
+    // Methods
+    void start() {
+        System.out.println("The bike moves at speed 100 kmph");
+    }
+}
---
+++ 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/tns.backend.training/src/tns/backend/training/bikemain.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+    // create a demo of a class
+    bike b = new bike();
+    b.name = "Splendor";
+    b.speed = 180;       // speed should be an integer, not a string
+    b.model = "Petrol";
+
+    bike b1 = new bike();
+    b1.name = "Pulsar";
+    b1.model = "Petrol";
+    b1.speed = 100;
+}
