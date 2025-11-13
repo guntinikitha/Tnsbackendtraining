7c874b56e357f8d7e711f463b105b5645a49379a
Gunti Nikitha <Gunti Nikitha@LAPTOP-ER6FN7S0>
2025-11-13 09:24:07 +0530

    Pushing the code

---
+++ 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/tns.backend.training/src/tns/backend/training/car.java
@@ -0,0 +1,14 @@
+package tns.backend.training;
+
+public class car {
+	//variables
+	int speed;
+	String model;
+	String name;
+	//methods
+	void start() {
+		System.out.println("The car moves speed 140kmph");
+
+	}
+
+}
---
+++ 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/tns.backend.training/src/tns/backend/training/carmain.java
@@ -0,0 +1,24 @@
+package tns.backend.training;
+
+// Executor class for Car
+	public class carmain {
+	    public static void main(String[] args) {
+	        // Create instance of a class
+	        // ClassName objectName = new Constructor();
+
+	    	
+	    	         car c = new car();
+	    	         c.name = "Honda City";
+	    	        c.model = "Petrol";
+	    	        c.speed = 160;
+
+	    	        car c1 = new car();
+	    	        c1.name = "Honda Amaze";
+	    	        c1.model = "Diesel";
+	    	        c1.speed = 140;
+
+
+	    }
+	}
+
+
